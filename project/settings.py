9cea5fbb87b65adbdc66d2d731b96a2ee4e61dc6
Nitin Chahar <nitin@agrexai.com>
2019-08-07 16:57:19 +0530

    code refactor

--- project/settings.py
+++ project/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'Gate_Entry_Exit',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+         'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'PORT': '5432',
 
     }
 }
